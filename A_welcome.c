e4645655b24addf852892ec137e3f7e771d260a3
MD. FAZLA RABBI <fazlarabbiabir4@gmail.com>
2026-01-05 22:49:54 +0600

    Create A_welcome.c

---
+++ A_welcome.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  //'stdio' = standard input output & '.h' = header file extension 
+
+int main() //main function
+{
+    printf("Assalamualaikum."); //printf() = console print function
+    printf("I'm Fazla Rabbi Abir. \n"); // '\n' = escape sequence, means print new line
+    printf("I passed my HSC in Noakhali Govt College.");
+
+    return 0; //successful termination
+}
