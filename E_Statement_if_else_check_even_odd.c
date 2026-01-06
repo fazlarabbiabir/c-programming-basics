8cf32ee90a9c4f819e3527533444cf15f8328c81
MD. FAZLA RABBI <fazlarabbiabir4@gmail.com>
2026-01-06 11:56:00 +0600

    Create E_Statement_if_else_check_even_odd.c

---
+++ E_Statement_if_else_check_even_odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+
+    if(num%2 == 0)
+    {
+        printf("This is even number.");
+    }
+    else
+    {
+        printf("This is odd number.");
+    }
+}
